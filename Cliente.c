4c4f6e76e337236230a86db659e17ffd07e29f73
jovnascimento <67511057+jovnascimento@users.noreply.github.com>
2021-07-23 17:31:39 -0300

    Cliente/Servidor v2
    Segunda versão do código

--- Cliente.c
+++ Cliente.c
@@ -1,111 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <strings.h>
-#include <windows.h>
-#include <winsock.h>
+#include <string.h>
 
-int main(int argc, char *argv[]){
-    WSADATA wsaData;
-    LPHOSTENT hostEntry;
+//Headers de socket
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-    int socks, rc, i, fd, choice = 1;
-    char afileName[2000], vfileName[2000], file_buffer[2000],c,caufile[70000],aufile[7000000],vfile[1000000];
-    struct sockaddr_in	cliAddr, remoteServAddr;
+int main(){
+	int sd;
+	char fileName[2000], file_buffer[2000],c,caufile[70000],audio[50000],video[50000];
 
-    WSAStartup(MAKEWORD(2,1), &wsaData);
+	struct sockaddr_in remoteServAddr;
 
-    hostEntry = gethostbyname(argv[1]);
-    if(hostEntry == NULL){
-        printf("Host desconhecido %s\n", argv[1]);
-        return 1;
-    }
+	//Criando socket
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sd < 0){
+	    printf("Erro na criação do socket\n");
+	    exit(1);
+	}
 
-    // Creating socket file descriptor
-    if ( (fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-        perror("Erro");
-        exit(EXIT_FAILURE);
-    }
+    //Limpando o remoteServAddr
+	memset(&remoteServAddr, 0, sizeof(remoteServAddr));
 
-    memset(&remoteServAddr, 0, sizeof(remoteServAddr));
+	//Adicionando informações do server
+	remoteServAddr.sin_family = AF_INET;
+	remoteServAddr.sin_port = htons(8000);
+	remoteServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+    char num = 1;
+    sendto(sd, &num, sizeof(num), 0,(struct sockaddr *)&remoteServAddr, sizeof(struct sockaddr));
 
-    // Filling server information
-    remoteServAddr.sin_family = AF_INET;
-    remoteServAddr.sin_port = htons(8000);
-    remoteServAddr.sin_addr.s_addr = INADDR_ANY;
+    printf("Digite o nome do arquivo: \n");
+    scanf("%s",fileName);
+    sendto(sd, fileName, strlen(fileName), 0,(struct sockaddr *)&remoteServAddr, sizeof(struct sockaddr));
 
-    while(choice != 4)
-    {
-        printf("          MENU          ");
-        printf("1. Musica");
-        printf("2. Video");
-        printf("3. Sair");
-        scanf("%d", &choice);
+    FILE *fp;
+    fp=fopen(fileName,"r");
 
-        char num = choice;
-
-        sendto(fd, &num, sizeof(num), 0,(struct sockaddr *)&remoteServAddr, sizeof(struct sockaddr));
-
-        switch(choice)
-        {
-            case 1:
-                printf("Digite o nome do arquivo de audio que deseja:\n");
-                scanf("%s",afileName);
-                sendto(fd, afileName, strlen(afileName), 0,(struct sockaddr *)&remoteServAddr, sizeof(struct sockaddr));
-                FILE *afp;
-                afp=fopen(afileName,"r");
-                fseek(afp,0,SEEK_END);
-                size_t  afsize=ftell(afp);
-                fseek(afp,0,SEEK_SET);
-
-                if(afp){
-                    printf("Lendo arquivo\n");
-                    if(fread(aufile,afsize,1,afp)<=0){
-                        printf("Nao foi possivel ler o arquivo ou copia-lo ao buffer\n");
-                        exit(1);
-                    }
-                }
-                else{
-                    printf("Nao foi possivel ler o arquivo\n");
-                    exit(0);
-                }
-
-                if(sendto(fd, aufile, afsize, 0,(struct sockaddr *)&remoteServAddr, sizeof(struct sockaddr))<0)
-                    printf("Arquivo nao pode ser enviado\n");
-                else
-                    printf("Arquivo enviado com sucesso");
-                fclose(afp);
-                break;
-
-            case 2:
-                printf("Digite o nome do arquivo de video que deseja:\n");
-                scanf("%s",vfileName);
-                sendto(fd, vfileName, strlen(vfileName), 0,(struct sockaddr *)&remoteServAddr, sizeof(struct sockaddr));
-                FILE *vfp;
-                vfp=fopen(vfileName,"r");
-
-                fseek(vfp, 0, SEEK_END);
-                size_t vfsize = ftell(vfp);
-                fseek(vfp, 0, SEEK_SET);
-
-                if(vfp){
-                    if(fread(vfile, 1, vfsize, vfp)<=0)
-                        printf("Nao foi possivel ler o arquivo\n");
-                }
-                else{
-                    printf("Nao foi possivel ler o arquivo\n");
-                    exit(0);
-                }
-                if(sendto(fd, vfile, vfsize, 0,(struct sockaddr *)&remoteServAddr, sizeof(struct sockaddr))<0)
-                    printf("Arquivo nao pode ser enviado");
-                else
-                    printf("Arquivo enviado com sucesso");
-                fclose(vfp);
-                break;
-
-            case 3:
-                close(fd);
-                break;
+    if(fp){
+        printf("Reading file contents.\n");
+     	fseek(fp,0,SEEK_END);
+       	size_t file_size=ftell(fp);
+       	fseek(fp,0,SEEK_SET);
+      	if(fread(file_buffer,file_size,1,fp)<=0){
+           	printf("Unable to copy file into buffer or empty file.\n");
+           	exit(1);
         }
     }
+    else{
+    	printf("Cannot open file.\n");
+    	exit(0);
+    }
+    printf("FILE CONTENTS TO SEND : %s\n",file_buffer);
+    if(sendto(sd, file_buffer, strlen(file_buffer), 0,(struct sockaddr *)&remoteServAddr, sizeof(struct sockaddr))<0){
+    	printf("FILE WAS NOT SENT\n");
+    }
+    else{
+    	printf("FILE SENT\n");
+    }
+    fclose(fp);
 }
