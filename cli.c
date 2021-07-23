6b400903d3898d4df10ea98f02fe29d2f28a0889
Jubeto Oliveira <jubetooliveira@gmail.com>
2021-07-23 17:02:46 -0300

    Após receber a solicitação do cliente, o servidor devolve uma resposta para ele

--- cli.c
+++ cli.c
@@ -3,8 +3,71 @@
 #include <stdio.h>
 #include <string.h>
 
+#define LOCAL_SERVER_PORT 1501
 #define REMOTE_SERVER_PORT 1500
 #define MAX_MSG 100
+
+void recebe_resposta(){
+    WSADATA wsaData;
+
+    int socks, rc, n, cliLen;
+    struct sockaddr_in cliAddr, servAddr;
+    char msg[MAX_MSG];
+
+    // INICIALIZA A DLL DE SOCKETS PARA O WINDOWS
+    WSAStartup(MAKEWORD(2,1),&wsaData);
+
+    // CRIACAO DO SOCKET
+    socks = socket(AF_INET, SOCK_DGRAM, 0);
+    if(socks < 0) {
+        printf("\nValor de socks: %d", socks);
+        printf("Socket nao pode ser aberto\n");
+        return;
+    }
+
+    // VINCULAR A PORTA DO SERVIDOR
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(LOCAL_SERVER_PORT);
+
+    // CRIAcaO DO SOCKET
+    socks = socket(AF_INET, SOCK_DGRAM, 0);
+    if(socks < 0) {
+        printf("Socket nao pode ser aberto\n");
+        return;
+    }
+
+    // TESTA SE A PORTA ESTA DISPONIVEL
+    rc = bind (socks, (struct sockaddr *) &servAddr,sizeof(servAddr));
+    if(rc<0) {
+        printf("Vinculo com numero de porta impossibilitado %d \n", LOCAL_SERVER_PORT);
+        return;
+    }
+
+    printf("Esperando pelos dados na porta UDP %u\n",LOCAL_SERVER_PORT);
+
+    // LACO INFINITO DE ESPERA DO SERVIDOR
+    while(1) {
+
+        // INICIANDO BUFFER
+        memset(msg,0x0,MAX_MSG);
+
+        // RECEBENDO UMA MENSGAEM
+        cliLen = sizeof(cliAddr);
+        n = recvfrom(socks, msg, MAX_MSG, 0, (struct sockaddr *) &cliAddr, &cliLen);
+
+        if(n<0) {
+          printf("Nao pode receber os dados \n");
+        }
+
+        // IMPRIMIR MENSAGEM RECEBIDA
+        printf("De %s:UDP%u : %s \n", inet_ntoa(cliAddr.sin_addr), ntohs(cliAddr.sin_port),msg);
+
+    } // FIM DO LOOP DO SERVIDOR
+
+    closesocket(socks);
+    WSACleanup();
+}
 
 int main(int argc, char *argv[]) {
 	char nome_arquivo[100];
@@ -23,7 +86,7 @@ int main(int argc, char *argv[]) {
     // VALIDA ENDERECO DE IP RECEBIDO COMO ARGUMENTO
     hostEntry = gethostbyname("127.0.0.1");
     if (hostEntry == NULL){
-       printf("Host desconhecido %s\n", argv[1]);
+       printf("Host desconhecido 127.0.0.1\n");
        return 1;
     }
 
@@ -63,6 +126,7 @@ int main(int argc, char *argv[]) {
 
     closesocket(socks);
     WSACleanup();
+    recebe_resposta();
 
     return 0;
 }
--- se.c
+++ se.c
@@ -3,7 +3,69 @@
 #include <stdio.h>
 
 #define LOCAL_SERVER_PORT 1500
+#define REMOTE_SERVER_PORT 1501
 #define MAX_MSG 100
+
+void envia_resposta(char endereco[], unsigned int porta, char mensagem[]){
+    printf("De %s:UDP%u : %s \n", endereco, porta, mensagem);
+
+    char resposta[100];
+	printf("Digite a resposta: ");
+    scanf("%s", &resposta);
+	
+    WSADATA wsaData;
+    LPHOSTENT hostEntry;
+
+    int socks, rc, i;
+    struct sockaddr_in cliAddr, remoteServAddr;
+
+    // INICIALIZA A DLL DE SOCKETS PARA O WINDOWS
+    WSAStartup(MAKEWORD(2,1),&wsaData);
+
+    // VALIDA ENDERECO DE IP RECEBIDO COMO ARGUMENTO
+    hostEntry = gethostbyname("127.0.0.1");
+    if (hostEntry == NULL){
+       printf("Host desconhecido 127.0.0.1\n");
+       return;
+    }
+
+    // VINCULAR A PORTA DO SERVIDOR REMOTO
+    remoteServAddr.sin_family = hostEntry->h_addrtype ;
+    remoteServAddr.sin_addr = *((LPIN_ADDR)*hostEntry->h_addr_list);
+    remoteServAddr.sin_port = htons(REMOTE_SERVER_PORT);	// NUMERO DA PORTA VINDA PELA LINHA DE COMANDO
+
+    // CRIANDO SOCKET
+    socks = socket(AF_INET,SOCK_DGRAM,0);
+    if(socks < 0) {
+        printf("Socket nao pode ser aberto\n");
+        return;
+    }
+
+    /* VINCULAR A PORTA DO CLIENTE */
+    cliAddr.sin_family = AF_INET;
+    cliAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    cliAddr.sin_port = htons(0);
+
+    rc = bind(socks, (struct sockaddr *) &cliAddr, sizeof(cliAddr));
+    if(rc<0) {
+        printf("Nao pode vincular a porta\n");
+        return;
+    }
+
+    // ENVIANDO OS DADOS    
+    rc = sendto(socks, resposta, strlen(resposta)+1, 0,
+        (LPSOCKADDR) &remoteServAddr,
+        sizeof(struct sockaddr));
+
+    if(rc<0) {
+      printf("Nao pode enviar dados %d \n",i-1);
+      closesocket(socks);
+      return;
+    }
+
+    closesocket(socks);
+    WSACleanup();
+}
 
 int main(int argc, char *argv[]) {
     WSADATA wsaData;
@@ -59,12 +121,11 @@ int main(int argc, char *argv[]) {
         }
 
         // IMPRIMIR MENSAGEM RECEBIDA
-        printf("De %s:UDP%u : %s \n",
-               inet_ntoa(cliAddr.sin_addr), ntohs(cliAddr.sin_port),msg);
+        envia_resposta(inet_ntoa(cliAddr.sin_addr), ntohs(cliAddr.sin_port),msg);
 
-        } // FIM DO LOOP DO SERVIDOR
+    } // FIM DO LOOP DO SERVIDOR
 
-        closesocket(socks);
-        WSACleanup();
-        return 0;
+    closesocket(socks);
+    WSACleanup();
+    return 0;
 }
