c36ba3107d789f8f3f55d67822cd7dc1030af177
Jubeto Oliveira <jubetooliveira@gmail.com>
2021-07-24 15:25:51 -0300

    Juntando Funções semelhantes
    envia_resposta_c2 e envia_resposta

--- cli.c
+++ cli.c
@@ -58,100 +58,6 @@ int ler_arquivo(t_arquivo aux_arquivos[MAX_MSG]){
 	}
 }
 
-void envia_resposta_c2(char mensagem[], int tipo){
-    //tipo 0 cliente enviar a solicitacao do arquivo para o servidor
-    //tipo 1 cliente enviar a solicitação do arquivo para outro cliente
-    //tipo 2 cliente que possui o arquivo enviar resposta para o cliente que pediu o arquivo
-    int j, k, porta;
-
-    WSADATA wsaData;
-    LPHOSTENT hostEntry;
-
-    int socks, rc, i;
-    struct sockaddr_in cliAddr, remoteServAddr;
-
-    char vet_resposta[MAX_MSG][16];
-    if(tipo == 2){
-        t_arquivo vet_arquivos[MAX_MSG];
-        
-        int len_vet = ler_arquivo(vet_arquivos);
-
-        for(k = 0; k<MAX_MSG; k++)//inicializando o vetor de respostas com strings vazias
-            strcpy(vet_resposta[k], "");
-
-        // inserindo no vetor de respostas os ips dos elementos do vetor vet_arquivos que possuem o arquivo com nome desejado
-        k=0;
-        for(j= 0; j < len_vet; j++){
-            if(strcmp(vet_arquivos[j].nome,mensagem) == 0){
-                fflush(stdin);
-                strcpy(vet_resposta[k], vet_arquivos[j].ip);
-                k++;
-            }
-        }
-        strcpy(vet_resposta[k], "FIM");
-    }
-
-    // INICIALIZA A DLL DE SOCKETS PARA O WINDOWS
-    WSAStartup(MAKEWORD(2,1),&wsaData);
-
-    // VALIDA ENDERECO DE IP RECEBIDO COMO ARGUMENTO
-    hostEntry = gethostbyname("127.0.0.1");
-    if (hostEntry == NULL){
-       printf("Host desconhecido 127.0.0.1\n");
-       return;
-    }
-
-    // VINCULAR A PORTA DO SERVIDOR REMOTO
-    if(tipo == 2)   porta = LOCAL_CLIENT_PORT;
-    if(tipo == 1)   porta = REMOTE_CLIENT_PORT;
-    remoteServAddr.sin_family = hostEntry->h_addrtype ;
-    remoteServAddr.sin_addr = *((LPIN_ADDR)*hostEntry->h_addr_list);
-    remoteServAddr.sin_port = htons(porta);	// NUMERO DA PORTA VINDA PELA LINHA DE COMANDO
-
-    // CRIANDO SOCKET
-    socks = socket(AF_INET,SOCK_DGRAM,0);
-    if(socks < 0) {
-        printf("Socket nao pode ser aberto\n");
-        return;
-    }
-
-    /* VINCULAR A PORTA DO CLIENTE */
-    cliAddr.sin_family = AF_INET;
-    cliAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    cliAddr.sin_port = htons(0);
-
-    rc = bind(socks, (struct sockaddr *) &cliAddr, sizeof(cliAddr));
-    if(rc<0) {
-        printf("Nao pode vincular a porta\n");
-        return;
-    }
-
-    // ENVIANDO OS DADOS
-    if(tipo == 2){
-        for(k=0; k<MAX_MSG; k++){
-            if(strcmp(vet_resposta[k],"") != 0){
-                rc = sendto(socks, vet_resposta[k], strlen(vet_resposta[k])+1, 0,(LPSOCKADDR) &remoteServAddr, sizeof(struct sockaddr));
-                if(rc<0) {
-                    printf("Nao pode enviar dados %d \n",i-1);
-                    closesocket(socks);
-                    return;
-                }    
-            }else   break;
-        }
-    }
-    if(tipo == 1){
-        rc = sendto(socks, mensagem, strlen(mensagem)+1, 0,
-            (LPSOCKADDR) &remoteServAddr,
-            sizeof(struct sockaddr));
-        rc = sendto(socks, "FIM", 4, 0,
-            (LPSOCKADDR) &remoteServAddr,
-            sizeof(struct sockaddr));
-    }
-
-    closesocket(socks);
-    WSACleanup();
-}
-
 void recebe_resposta_c2(char vet_resposta[MAX_MSG][MAX_MSG]){
     WSADATA wsaData;
 
@@ -365,14 +271,38 @@ void recebe_resposta(char vet_resposta[MAX_MSG][MAX_MSG]){
     WSACleanup();
 }
 
-void envia_resposta(char mensagem[]){
-    int j, k;
-	
+void envia_resposta(char mensagem[], int tipo){
+    //tipo 0 cliente enviar a solicitacao do arquivo para o servidor
+    //tipo 1 cliente enviar a solicitação do arquivo para outro cliente
+    //tipo 2 cliente que possui o arquivo enviar resposta para o cliente que pediu o arquivo
+    int j, k, porta;
+
     WSADATA wsaData;
     LPHOSTENT hostEntry;
 
     int socks, rc, i;
     struct sockaddr_in cliAddr, remoteServAddr;
+
+    char vet_resposta[MAX_MSG][16];
+    if(tipo == 2){
+        t_arquivo vet_arquivos[MAX_MSG];
+        
+        int len_vet = ler_arquivo(vet_arquivos);
+
+        for(k = 0; k<MAX_MSG; k++)//inicializando o vetor de respostas com strings vazias
+            strcpy(vet_resposta[k], "");
+
+        // inserindo no vetor de respostas os ips dos elementos do vetor vet_arquivos que possuem o arquivo com nome desejado
+        k=0;
+        for(j= 0; j < len_vet; j++){
+            if(strcmp(vet_arquivos[j].nome,mensagem) == 0){
+                fflush(stdin);
+                strcpy(vet_resposta[k], vet_arquivos[j].ip);
+                k++;
+            }
+        }
+        strcpy(vet_resposta[k], "FIM");
+    }
 
     // INICIALIZA A DLL DE SOCKETS PARA O WINDOWS
     WSAStartup(MAKEWORD(2,1),&wsaData);
@@ -385,9 +315,12 @@ void envia_resposta(char mensagem[]){
     }
 
     // VINCULAR A PORTA DO SERVIDOR REMOTO
+    if(tipo == 2)   porta = LOCAL_CLIENT_PORT;
+    if(tipo == 1)   porta = REMOTE_CLIENT_PORT;
+    if(tipo == 0)   porta = REMOTE_SERVER_PORT;
     remoteServAddr.sin_family = hostEntry->h_addrtype ;
     remoteServAddr.sin_addr = *((LPIN_ADDR)*hostEntry->h_addr_list);
-    remoteServAddr.sin_port = htons(REMOTE_SERVER_PORT);	// NUMERO DA PORTA VINDA PELA LINHA DE COMANDO
+    remoteServAddr.sin_port = htons(porta);	// NUMERO DA PORTA VINDA PELA LINHA DE COMANDO
 
     // CRIANDO SOCKET
     socks = socket(AF_INET,SOCK_DGRAM,0);
@@ -408,12 +341,25 @@ void envia_resposta(char mensagem[]){
     }
 
     // ENVIANDO OS DADOS
-    rc = sendto(socks, mensagem, strlen(mensagem)+1, 0,
-        (LPSOCKADDR) &remoteServAddr,
-        sizeof(struct sockaddr));
-    rc = sendto(socks, "FIM", 4, 0,
-        (LPSOCKADDR) &remoteServAddr,
-        sizeof(struct sockaddr));
+    if(tipo == 2){
+        for(k=0; k<MAX_MSG; k++){
+            if(strcmp(vet_resposta[k],"") != 0){
+                rc = sendto(socks, vet_resposta[k], strlen(vet_resposta[k])+1, 0,(LPSOCKADDR) &remoteServAddr, sizeof(struct sockaddr));
+                if(rc<0) {
+                    printf("Nao pode enviar dados %d \n",i-1);
+                    closesocket(socks);
+                    return;
+                }    
+            }else   break;
+        }
+    }else{ //tipo 0 e 1 enviam o mesmo tipo de mensagem
+        rc = sendto(socks, mensagem, strlen(mensagem)+1, 0,
+            (LPSOCKADDR) &remoteServAddr,
+            sizeof(struct sockaddr));
+        rc = sendto(socks, "FIM", 4, 0,
+            (LPSOCKADDR) &remoteServAddr,
+            sizeof(struct sockaddr));
+    }
 
     closesocket(socks);
     WSACleanup();
@@ -427,7 +373,7 @@ int main(int argc, char *argv[]) {
     int opcao = 1;
 
     while(opcao != 3){
-        printf("\tMenu\nEscolha uma opcao: \n1. Enviar arquivo\n2. Receber arquivo\n3. Sair\n");
+        printf("\tMenu\nEscolha uma opcao: \n1. Solicitar arquivo\n2. Enviar arquivo\n3. Sair\n");
         
         printf("Digite a opcao que deseja: ");
         scanf("%d", &opcao);
@@ -440,7 +386,7 @@ int main(int argc, char *argv[]) {
                 printf("Digite o nome do arquivo: ");
                 scanf("%s", &nome_arquivo);
 
-                envia_resposta(nome_arquivo);
+                envia_resposta(nome_arquivo,0);
 
                 recebe_resposta(vet_resposta);
 
@@ -450,7 +396,7 @@ int main(int argc, char *argv[]) {
                     else break;
                 }
 
-                envia_resposta_c2(nome_arquivo, 1);
+                envia_resposta(nome_arquivo, 1);
             break;
 
             case 2:
@@ -468,7 +414,7 @@ int main(int argc, char *argv[]) {
                 }
 
                 if(k > 0){
-                    envia_resposta_c2(vet_resposta[0], 2);
+                    envia_resposta(vet_resposta[0], 2);
                 }
             break;
 
