4db2d3abd5239fb4b4d1914de6348b00822b9429
Jubeto Oliveira <jubetooliveira@gmail.com>
2021-07-24 21:58:20 -0300

    lendo arquivo em pacotes de 50 caracteres

--- arq.c
+++ arq.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int le_arquivo(char nome_arquivo[20],  char resultado[100000]){
-    int i = 0;
+int le_arquivo(char nome_arquivo[20],  char resultado[20000][50]){
+    int i = 0, j =0, k=0;
     char ch;
     FILE *source;
     source = fopen(nome_arquivo, "r");
@@ -12,31 +12,41 @@ int le_arquivo(char nome_arquivo[20],  char resultado[100000]){
     }
     
     while( ( ch = fgetc(source) ) != EOF ){
-        resultado[i] = ch;
-        i++;
+        resultado[i][j] = ch;
+        j++;
+        if(j==50){
+            i++;
+            j = 0;
+        }
+        k++;
     }
     fclose(source);
-    return i;
+    return k;
 }
 
-void escreve_arquivo(char novo_arquivo[20], int n,  char resultado[100000]){
-    int i = 0;
+void escreve_arquivo(char novo_arquivo[20], int n,  char resultado[20000][50]){
+    int i = 0, j = 0, k=0;
     FILE *target;
     target = fopen(novo_arquivo, "w");
     if( target == NULL ){
         printf("Erro ao ler o arquivo\n");
         exit(EXIT_FAILURE);
     }
-    while(i < n){
-        fputc(resultado[i], target);
-        i++;
+    while(k < n){
+        fputc(resultado[i][j], target);
+        j++;
+        if(j==50){
+            i++;
+            j = 0;
+        }
+        k++;
     }
     
 }
 
 
 int main(){
-    char ch, nome_arquivo[20], novo_arquivo[20], resultado[100000];
+    char ch, nome_arquivo[20], novo_arquivo[20], resultado[20000][50];
     int i;
     printf("Digite o nome do arquivo a ser copiado\n");
     scanf("%s", &nome_arquivo);
